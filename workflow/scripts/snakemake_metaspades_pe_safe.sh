05ffff38d4375616de60f72114659ad418c04c21
Vi <49007003+V-Varga@users.noreply.github.com>
2025-01-13 14:31:09 +0100

    Update snakemake_metaspades_pe_safe.sh
    fixed timeout time to 6h

--- workflow/scripts/snakemake_metaspades_pe_safe.sh
+++ workflow/scripts/snakemake_metaspades_pe_safe.sh
@@ -55,8 +55,7 @@ if [ -s results/Assembly/PerSample/Assembly_INCOMPLETE_PE.TXT ]; then
 		file_base_id="${file_base2%_norm}"; #this removes the "_norm" substring
 		mkdir -p results/Assembly/PerSample/${parentname}/${file_base_id}; #create an output directory
 		# now run the program, with a time limit of 6 hours for successful assembly
-		#timeout 6h apptainer exec workflow/containers/metagenome_assembly.sif metaspades.py \
-		timeout 20m apptainer exec workflow/containers/metagenome_assembly.sif metaspades.py \
+		timeout 6h apptainer exec workflow/containers/metagenome_assembly.sif metaspades.py \
 		-1 $file -2 results/DataNonHuman/100k_Filt/${parentname}/${file_base_id}_norm.2.fq \
 		--checkpoints all --threads $thread_count \
 		-o results/Assembly/PerSample/${parentname}/${file_base_id}/;
